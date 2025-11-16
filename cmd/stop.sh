19f74d194de098d2958785d7211e42217d63b820
xujialiu <xujialiuphd@gmail.com>
2025-11-16 17:31:04 +0800

    add: new script for stop processes runned specific user

--- cmd/stop.sh
+++ cmd/stop.sh
@@ -1,7 +1,8 @@
 for pid in $(pstree -p 888888 | grep -o '([0-9]\+)' | tr -d '()'); do 
-    kill -STOP $pid
+    kill -STOP $pid # -CONT to resume
 done
 
-for pid in $(pstree -p 888888 | grep -o '([0-9]\+)' | tr -d '()'); do 
-    kill -CONT $pid
+
+for pid in $(ps -u user -o pid --no-headers); do 
+    kill -STOP $pid  # -CONT to resume
 done
